1887ec8a991a50b47eeae044f7a9e0cf1f393a2e
Stone <stoneyan1995@gmail.com>
2019-09-09 19:42:06 -0400

    Updates

---
+++ precache-manifest.c51d4a7db40a92453ccfb65165d7b272.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92ef9f344bd1a7dae53a0735da365fdc",
+    "url": "/client-ui-Trello/index.html"
+  },
+  {
+    "revision": "f1ebb3e625167c1474d1",
+    "url": "/client-ui-Trello/static/css/main.a7d723ba.chunk.css"
+  },
+  {
+    "revision": "2caded59ddca983377fa",
+    "url": "/client-ui-Trello/static/js/2.91b49064.chunk.js"
+  },
+  {
+    "revision": "f1ebb3e625167c1474d1",
+    "url": "/client-ui-Trello/static/js/main.70916298.chunk.js"
+  },
+  {
+    "revision": "3326e7eaf18e14e82c92",
+    "url": "/client-ui-Trello/static/js/runtime~main.2f4b99d7.js"
+  }
+]);
